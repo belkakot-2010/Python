714815e249ae05419eea9c11f9b65709c919a9ea
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2023-10-03 19:12:06 +0300

    Add files via upload

---
+++ задача с числами.py
@@ -0,0 +1,18 @@
+num = 0
+
+while num < 101:   
+    
+    if num % 15 == 0:
+        print('баз, физ')        
+    
+    elif num % 3 == 0:
+        print('физ')
+    
+    elif num % 5 == 0:
+        print('баз')
+        
+    else:
+        print(num)
+    num += 1     
+   
+    
