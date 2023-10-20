2f95834992578495af7d4b2ff0603048cfd5c811
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2023-10-20 19:42:23 +0300

    Add files via upload

---
+++ school.py
@@ -0,0 +1,34 @@
+'''
+Занятия начинаются в 8.30
+урок длится 45 минут
+перемена между уроками - 10 мин.
+программа спрашивает, какой сейчас урок
+программа спрашивает врема, которое шли занятия:
+часы, минуты
+
+Примеры:
+
+1
+9:15
+
+2
+10:10
+
+3
+11:5
+'''
+
+num_les = int(input('Сколько было уроков? '))
+peremena = (num_les - 1) * 10
+
+time = num_les * 45
+time += peremena
+time += 510
+
+hours = time // 60
+min = time % 60
+if min < 10:
+    plohaya_minuta = '0' + str(min)
+    print(hours, plohaya_minuta, sep=':')
+else:
+    print(hours, min, sep=':')
