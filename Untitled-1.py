b1790631893bd1babdfb9deb36f8754269be39ff
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2024-02-02 19:42:08 +0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -9,25 +9,54 @@
 Ничья - нет возможности создать выигрышную комбинацию.
 '''
 
+from os import system
+
+
 BatleField = ['.'] * 9
 
 
-def draw_field(Batlefield: list) -> None:
-    for i in range(0, 2, 7):
-        print(Batlefield[i], Batlefield[i + 1], Batlefield[i + 1])
+def draw_field(Batlefield: list) -> str:
+    system('cls')
+    for i in range(0, 7, 3):
+        print(Batlefield[i], Batlefield[i + 1], Batlefield[i + 2])
 
 
-def make_move(BatleField: list, player: str):
+def make_move_human(BatleField: list, player: str) -> None:
     while True:
         cell_num = int(input('Выберете клетку в которую сделаете ход '))
         if cell_num < 1 or cell_num > 9:
             print('Ошибка! Номер должен быть от 1 до 9')
             continue
-        if BatleField[cell_num] in players:
+        if BatleField[cell_num - 1] in players:
             print('Ошибка! Клетка занята')
             continue
-        BatleField[cell_num] = player
+        BatleField[cell_num - 1] = player
         break
+
+
+def make_move_comp(BatleField: list, player: str) -> None:
+    cells_index = []
+    for i in range(9):
+        if BatleField[i] == '.':
+            BatleField.append(i)
+
+
+def get_winner(BatleField: list, player: str) -> str:
+    for i in range(0, 7, 3):
+        if BatleField[i] == BatleField[i + 1] == BatleField[i + 2] == player:
+            return player
+
+    for i in range(0, 3, 1):
+        if BatleField[i] == BatleField[i + 3] == BatleField[i + 6] == player:
+            return player
+
+    if BatleField[0] == BatleField[4] == BatleField[8] == player:
+        return player
+    
+    if BatleField[2] == BatleField[4] == BatleField[6] == player:
+        return player
+
+    return ''
 
 
 player_1 = 'x'
@@ -36,12 +65,22 @@ players = (player_1, player_2)
 moves = 1
 
 while True:
+    if moves > 9:
+        draw_field(BatleField)
+        print('Ничья')
+        break
     draw_field(BatleField)
     if moves % 2:
-        make_move(BatleField, player_1)
+        player = player_1
     else:
-        make_move(BatleField, player_2)
+        player = player_2
+    make_move_human(BatleField, player)
     moves += 1
+    winner = get_winner(BatleField, player)
+    if winner: 
+        draw_field(BatleField)
+        print('Победил', winner)
+        break
 
 
 print('Игра окончена')
