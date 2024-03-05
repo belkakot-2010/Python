0d397759f16140b64d14f3b9eba70669dce650a2
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2024-03-05 19:42:05 +0300

    Add files via upload

--- Cards.py
+++ Cards.py
@@ -1,37 +1,10 @@
 from random import shuffle
+from os import system
 
-'''
-Масти:
-    черви, пики, бубны, крести
-    
-Колода:
-    на каждую масть карты от 6 до туза = 36 карт
 
-Перетасовать колоду
-
-Игроки - от 2 до ...
-
-Каждому игроку дать по 2 карты
-
-Можно смотреть только свои карты
-
-Ход: беру ещё карту (сколько угодно, не больше, чем осталось в колоде)
-или закончить ход
-
-Каждый игрок делает один ход за партию
-
-Если сумма цен всех карт игрока больше 21, то он проиграл
-
-Если все игроки проиграли, то это ничья
-
-Победитель тот, кто набрал больше всего очков
-
-Если число очков равное, то будет ничья
-'''
-
-suits = ['Пики', 'Черви', 'Крести', 'Бубны']
-
-cards = {
+def get_deck() -> list[dict]:
+    suits = ['Пики', 'Черви', 'Крести', 'Бубны']
+    cards = {
     '6': 6,
     '7': 7,
     '8': 8,
@@ -42,43 +15,72 @@ cards = {
     'К': 4,
     'Т': 11
 }
+    deck = []
+    for suit in suits:
+        for item in cards:
+            card = {
+                'цена': cards[item],
+                'масть': suit,
+                'название': item
+            }
+            deck.append(card)
+    return deck
 
-deck = []
 
-for suit in suits:
-    for item in cards:
-        card = {
-            'цена': cards[item],
-            'масть': suit,
-            'название': item
-        }
-        deck.append(card)\
-        
-shuffle(deck)
-
-player_1 = {
+def get_players() -> list[dict]:
+    player_1 = {
         'имя': 'Дурик',
         'Карты': [],
-        'Человек': True
+        'Человек': True,
+        'Сумма': 0
     }
 
-player_2 = {
+    player_2 = {
         'имя': 'Дебик',
         'Карты': [],
-        'Человек': True
+        'Человек': True,
+        'Сумма': 0
     }
+    return [player_1, player_2]
 
-players = (player_1, player_2)
+
+def deal_cards(num: int) -> None:
+    for player in players:
+        for i in range(num):
+            player['Карты'].append(deck.pop())
+
+
+def show_cards() -> None:
+    for card in player['Карты']:
+        print(card['название'], card['масть'])
+
+
+deck = get_deck()     
+shuffle(deck)
+players = get_players()
+deal_cards(2)
+
+
 for player in players:
-    for i in range(2):
-        player['карты'].append(deck.pop())
+    while True:
+        system('cls')
+        show_cards()
+        player_option = input('Взять карту из колоды? y/n ')
+        if player_option.lower() == 'y':
+            player['Карты'].append(deck.pop())
+        else:
+            break
+
+
+system('cls')
 for player in players:
-    print(player[card])
+    for card in players[0]['Карты']:
+       player['Сумма'] += card['цена']
+    print(player['имя'], player['Сумма'])
 
 
-'''
-print('Карт в колоде', len(deck))
-for carg in deck:
-    print(card)
-    print('-' * 20)
-'''
+def get_winner(player):
+    if player['Сумма'] > 21:
+        print(player['имя'], 'Перебор')
+    else:
+        
