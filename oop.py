498a3245d77a9e0318e1afd8fd07256182de7090
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2024-04-05 19:40:16 +0300

    Add files via upload

--- oop.py
+++ oop.py
@@ -1,14 +1,23 @@
 '''
-ООП - стиль програмирования
-Класс - идея объекта
-Экземпляр - конкретная реализация идеи
-Переменные и функции внутри класс - атрибуты методы
+Cиситема уровня и опыта
+Награды за победы
+Инвентарь
 '''
 
 
+class Weapons:
+    '''Оружие'''
+    def __init__(self, name: str, attac: int) -> None:
+        self.name = name
+        self.attac = attac
+
+    def __str__(self) -> str:
+        return f'{self.name}, ({self.attac})'
+
+    
 class Player:  # определяем класс
     '''Игрок'''
-    def __init__(self, name: str, hp: int) -> None:
+    def __init__(self, name: str, hp: int, weapon=None) -> None:
         '''
         Конструктор класса
         Вызывается автоматически после создания экземпляра
@@ -17,38 +26,54 @@ class Player:  # определяем класс
         '''
         self.name = name  # атрибут
         self.hp = hp
-        self.power = 1
+        self.weapon_default = Weapons('Кулаки', 1)
+        if weapon:
+            self.weapon = weapon
+        else:
+            self.weapon = self.weapon_default
+        self.power = self.weapon.attac
 
     def __str__(self) -> str:
-        return f'Игрок {self.name}, жизни: {self.hp}'
+        '''Этот метод автоматически вызывается при обращении к объекту как к строке'''
+        return f'Игрок {self.name}, жизни: {self.hp}, оружие: {self.weapon}'
     
 
-    def attack(self, enemy):
-            enemy.hp -= self.power
-            print(self.name, 'атаковал', enemy.name)
-
+    def attack(self, enemy) -> None:
+        # ход игрока (игрок атакует противника)
+        if self.hp <= 0:
+            return
+        damage = self.power * self.weapon.attac
+        enemy.hp -= damage
+        print(self.name, 'атаковал', enemy.name)
 
     
-p1 = Player('Челик', 100)
-p2 = Player('Дурик', 101)
-print(p1)
-print(p2)
-    
-
 class Game:
+    '''Игра'''
     def __init__(self) -> None:
-        self.player = Player('Челик', 100)
-        self.enemy = Player('Дурик', 100)
+        self.player = Player('Челик', 100, Weapons('Меч кладенец', 5))
+        self.enemy = Player('Дурик', 100,)
+        self.is_fighting = False
         self.fight()
         
     def fight(self) -> None:
-        while True: 
+        '''Бой - обмен ударами'''
+        self.is_fighting = True
+        while self.is_fighting: 
             self.player.attack(self.enemy)
             print(self.player)
             self.enemy.attack(self.player)
             print(self.enemy)
-      
-        print('Бой окончен!')
+            self.check_winner()
+        print('Бой окончен')
+
+            
+    def check_winner(self) -> None:
+        if self.player.hp <= 0:
+            print(self.enemy.name, 'Победил')
+            self.is_fighting = False
+        elif self.enemy.hp <= 0:
+            print(self.player.name, 'Победил')
+            self.is_fighting = False
 
 
 Game()
