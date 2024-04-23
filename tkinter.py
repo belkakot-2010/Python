38616ac4647406f1bb1cff98f36c9ff1807c528b
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2024-04-23 19:45:39 +0300

    Add files via upload

--- tkinter.py
+++ tkinter.py
@@ -1,18 +1,7 @@
 import tkinter
 
-'''
-Вопрос: Label
-Ответы: Button
-Счёт: кол-во прввильных ответов
-'''
 
-'''
-текст вопрса
-варианты ответов
-правильный ответ
-'''
-
-question = [
+questions = [
     {
         'Текст вопроса': 'Чей Крым?',
         'Варианты ответов': ['Россия', 'Украина', 'Турция', 'Третий Рейх'],
@@ -37,19 +26,44 @@ question = [
         'Текст вопроса': 'Самая длинная река России?',
         'Варианты ответов': ['Волга', 'Москва', 'Лена', 'Колыма'],
         'индекс верного ответа': 2
-    }
+    },
+    {
+        'Текст вопроса': 'Какой из этих городов самый старый?',
+        'Варианты ответов': ['Москва', 'Королёв', 'Великий Новгород', 'Дербент'],
+        'индекс верного ответа': 3
+    },
+    {
+        'Текст вопроса': 'На какой реке находится Санкт-Петербург?',
+        'Варианты ответов': ['Нева', 'Москва', 'Анадырь', 'Кубань'],
+        'индекс верного ответа': 0
+    },
+    {
+        'Текст вопроса': 'Самая северная точка России?',
+        'Варианты ответов': ['Арбат', 'Мыс Дежнёва', 'Мыс Челюскин', 'Крым'],
+        'индекс верного ответа': 2
+    },
+    {
+        'Текст вопроса': 'Какой город был столицей России?',
+        'Варианты ответов': ['Ростов', 'Владимир', 'Нижний Новгород', 'Не один из перечисленных'],
+        'индекс верного ответа': 1
+    },
+    {
+        'Текст вопроса': 'Первый правитель России?',
+        'Варианты ответов': ['Рюрик', 'Пётр 1', 'Путин', 'Иван Грозный'],
+        'индекс верного ответа': 0
+    },
 ]
 
 
 class App:
     '''Приложение'''
-    def __init__(self, width: int, height: int) -> None:
+    def __init__(self) -> None:
         self.window = tkinter.Tk()
         self.window.option_add('*Font', ('Consolas', 30))
-        self.wight = width
-        self.height = height
-        self.window.geometry(f'{self.wight}x{self.height}')
-        self.question_num = None
+        self.width = self.window.winfo_screenwidth()
+        self.height = self.window.winfo_screenheight()
+        self.window.geometry(f'{self.width}x{self.height}')
+        self.question_number = None
         self.label_question_text = None
         self.label_answer_text_1 = None
         self.label_answer_text_2 = None
@@ -59,46 +73,92 @@ class App:
         self.button_answer_2 = None
         self.button_answer_3 = None
         self.button_answer_4 = None
-        self.make_widgets
-        self.position_widgets()
+        self.result_frame = None
+        self.make_widgets()
+        self.positon_widgets()
         self.start()
         self.window.mainloop()
 
-
     def make_widgets(self) -> None:
-        '''Создают экземпляры виджетов'''
-        self.label_question_text = tkinter.label(self.window)
-        self.label_answer_text_1 = tkinter.label(self.window)
-        self.label_answer_text_2 = tkinter.label(self.window)
-        self.label_answer_text_3 = tkinter.label(self.window)
-        self.label_answer_text_4 = tkinter.label(self.window)
-        self.button_answer_1 = tkinter.answer(self.window, text='1', command=self.on_clik(1))
-        self.button_answer_2 = tkinter.answer(self.window, text='2', command=self.on_clik(2))
-        self.button_answer_3 = tkinter.answer(self.window, text='3', command=self.on_clik(3))
-        self.button_answer_4 = tkinter.answer(self.window, text='4', command=self.on_clik(4))
+        '''Создает экземпляры виджетов'''
+        self.question_frame = tkinter.Frame(self.window)
+        self.label_question_text = tkinter.Label(self.question_frame)
+        self.answers_frame = tkinter.Frame(self.question_frame)
+        self.label_answer_text_1 = tkinter.Label(self.answers_frame)
+        self.label_answer_text_2 = tkinter.Label(self.answers_frame)
+        self.label_answer_text_3 = tkinter.Label(self.answers_frame)
+        self.label_answer_text_4 = tkinter.Label(self.answers_frame)
+        self.buttons_frame = tkinter.Frame(self.question_frame)
+        self.button_answer_1 = tkinter.Button(
+            self.buttons_frame, text='1', command=lambda: self.on_click(0)
+        )
+        self.button_answer_2 = tkinter.Button(
+            self.buttons_frame, text='2', command=lambda: self.on_click(1)
+        )
+        self.button_answer_3 = tkinter.Button(
+            self.buttons_frame, text='3', command=lambda: self.on_click(2)
+        )
+        self.button_answer_4 = tkinter.Button(
+            self.buttons_frame, text='4', command=lambda: self.on_click(3)
+        )
 
-    def position_widgets(self) -> None:
+    def positon_widgets(self) -> None:
         '''Позиционирует виджеты в окне'''
-        self.label_question_text.pack() 
-        self.label_answer_text_1.pack()
-        self.label_answer_text_2.pack()
-        self.label_answer_text_3.pack()
-        self.label_answer_text_4.pack()
-        self.button_answer_1.pack()
-        self.button_answer_2.pack()
-        self.button_answer_3.pack()
-        self.button_answer_4.pack()
+        self.question_frame.pack(expand=True)
+        self.label_question_text.pack(pady=35)
+        self.answers_frame.pack(pady=35)
+        self.label_answer_text_1.pack(anchor='w')
+        self.label_answer_text_2.pack(anchor='w')
+        self.label_answer_text_3.pack(anchor='w')
+        self.label_answer_text_4.pack(anchor='w')
+        self.buttons_frame.pack()
+        self.button_answer_1.pack(side='left', padx=25)
+        self.button_answer_2.pack(side='left', padx=25)
+        self.button_answer_3.pack(side='left', padx=25)
+        self.button_answer_4.pack(side='left', padx=25)
+
+    def show_question(self) -> None:
+        '''Показывает контент вопроса на экране'''
+        question = questions[self.question_number]
+        self.label_question_text['text'] = question['текст вопроса']
+        self.label_answer_text_1['text'] = '1. ' + question['варианты ответов'][0]
+        self.label_answer_text_2['text'] = '2. ' + question['варианты ответов'][1]
+        self.label_answer_text_3['text'] = '3. ' + question['варианты ответов'][2]
+        self.label_answer_text_4['text'] = '4. ' + question['варианты ответов'][3]
+
+    def on_click(self, button_index: int) -> None:
+        question = questions[self.question_number]
+        if button_index == question['индекс верного ответа']:
+            self.correct_answers += 1
+        else:
+            self.incorrect_answers += 1
+        self.question_number += 1
+        # Закончить викторину
+        if len(questions) == self.question_number:
+            self.show_result()
+        else:
+            self.show_question()
+
+    def show_result(self) -> None:
+        '''Показывает на экране результат викторины'''
+        self.question_frame.pack_forget()
+        self.result_frame = tkinter.Frame(self.window)
+        self.result_frame.pack()
+        tkinter.Label(self.result_frame, text='Викторина завершена!').pack()
+        tkinter.Label(self.result_frame, text=f'Всего вопросов: {len(questions)}').pack()
+        tkinter.Label(self.result_frame, text=f'Правильных ответов: {self.correct_answers}').pack()
+        tkinter.Label(self.result_frame, text=f'Ошибок: {self.incorrect_answers}').pack()
+        tkinter.Button(self.result_frame, text='начать заново', command=self.start).pack()
 
     def start(self) -> None:
-        self.question_num = 0
-        self.label_question_text['text'] = question[self.question_num]['Текст вопроса']
-        self.label_answer_text_1['text'] = '1.' + question[self.question_num]['Варианты ответов'][0]
-        self.label_answer_text_2['text'] = '2.' + question[self.question_num]['Варианты ответов'][1]
-        self.label_answer_text_3['text'] = '3.' + question[self.question_num]['Варианты ответов'][2]
-        self.label_answer_text_4['text'] = '4.' + question[self.question_num]['Варианты ответов'][3]
-
-    def on_clik(self, button_num) -> None:
-        print('Нажата кнопка', button_num)
+        '''Начинает викторину заново'''
+        self.question_number = 0
+        self.correct_answers = 0
+        self.incorrect_answers = 0
+        if self.result_frame:
+            self.result_frame.pack_forget()
+        self.question_frame.pack()
+        self.show_question()
 
 
 App()
