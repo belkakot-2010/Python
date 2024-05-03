746f5b8297de2fb8b07ce32907d960d76469e895
belkakot-2010 <145650496+belkakot-2010@users.noreply.github.com>
2024-05-03 19:35:15 +0300

    Add files via upload

--- TK.py
+++ TK.py
@@ -1,60 +1,9 @@
 import tkinter as tk
 import random
+import datetime
+import question
 
-
-questions = [
-    {
-        'Текст вопроса': 'Чей Крым?',
-        'Варианты ответов': ['Россия', 'Украина', 'Турция', 'Третий Рейх'],
-        'индекс верного ответа': 'Россия'
-    },
-    {
-        'Текст вопроса': 'Столица РФ?',
-        'Варианты ответов': ['Киев', 'Санкт-Петербург', 'Москва', 'Мухосранск'],
-        'индекс верного ответа': 'Москва'
-    },
-    {
-        'Текст вопроса': 'Cколько морей омывает РФ?',
-        'Варианты ответов': ['0', '7', '13', '12'],
-        'индекс верного ответа': '12'
-    },
-    {
-        'Текст вопроса': 'Самая высокая гора России?',
-        'Варианты ответов': ['Эльбрус', 'Белуха', 'Ключевская сопка', 'Кудыкина гора'],
-        'индекс верного ответа': 'Белуха'
-    },
-    {
-        'Текст вопроса': 'Самая длинная река России?',
-        'Варианты ответов': ['Волга', 'Москва', 'Лена', 'Колыма'],
-        'индекс верного ответа': 'Лена'
-    },
-    {
-        'Текст вопроса': 'Какой из этих городов самый старый?',
-        'Варианты ответов': ['Москва', 'Королёв', 'Великий Новгород', 'Дербент'],
-        'индекс верного ответа': 'Дербент'
-    },
-    {
-        'Текст вопроса': 'На какой реке находится Санкт-Петербург?',
-        'Варианты ответов': ['Нева', 'Москва', 'Анадырь', 'Кубань'],
-        'индекс верного ответа': 'Нева'
-    },
-    {
-        'Текст вопроса': 'Самая северная точка России?',
-        'Варианты ответов': ['Арбат', 'Мыс Дежнёва', 'Мыс Челюскин', 'Крым'],
-        'индекс верного ответа': 'Мыс Челюскин'
-    },
-    {
-        'Текст вопроса': 'Какой город был столицей России?',
-        'Варианты ответов': ['Ростов', 'Владимир', 'Нижний Новгород', 'Не один из перечисленных'],
-        'индекс верного ответа': 'Владимир'
-    },
-    {
-        'Текст вопроса': 'Первый правитель России?',
-        'Варианты ответов': ['Рюрик', 'Пётр 1', 'Путин', 'Иван Грозный'],
-        'индекс верного ответа': 'Рюрик'
-    },
-]
-
+questions = question.questions
 
 class App:
     def __init__(self, shuffle_question=False) -> None:
@@ -74,26 +23,35 @@ class App:
         self.incorrect_answers = None
         self.shuffle_question = None
 
+        self.start_time = None
+        self.end_time = None
+
         self.start()
         self.window.mainloop()
 
 
     def start(self) -> None:
+        self.start_time = datetime.datetime.now()
         self.question_index = 0
         self.correct_answers = 0
-        self.incorrect_answers = 0 
+        self.incorrect_answers = 0
         if self.shuffle_question:
             random.shuffle(questions)
         self.show_question()
 
+        for widget in self.main_frame.winfo_children():
+            widget.destroy()
+
     def show_question(self) -> None:
-        question = question[self.question_index]
-        tk.Label(self.main_frame, text=question['Текст вопроса']).pack()
+        question = questions[self.question_index]
+        tk.Label(self.main_frame, text=question['Текст вопроса']).pack(pady=(0, 30))
+        Buttons_frame = tk.Frame(self.main_frame)
+        Buttons_frame.pack()
         for answer in question['Варианты ответов']:
-            tk.Button(self.main_frame, text=answer, command= lambda arg=answer: self.on_button(arg)).pack
+            tk.Button(Buttons_frame='left', text=answer, command= lambda arg=answer: self.on_button(arg)).pack(side='left', padx=(0,15))
 
     def on_button(self, button_text) -> None:
-        question = question[self.question_index]
+        question = questions[self.question_index]
         if button_text == question['индекс верного ответа']:
             self.correct_answers += 1
         else:
@@ -109,9 +67,12 @@ class App:
             self.show_result
 
     def show_result(self) -> None:
-        tk.Label(self.main_frame, text=f'верно: {self.correct_answers}').pack
-        tk.Label(self.main_frame, text=f'неверно: {self.incorrect_answers}').pack
-        tk.Button(self.main_frame, text='заново' command=self.start()).pack
+        self.end_time = datetime.datetime.now()
+        total_time = self.start_time - self.end_time
+        tk.Label(self.main_frame, text=f'верно: {self.correct_answers}').pack()
+        tk.Label(self.main_frame, text=f'неверно: {self.incorrect_answers}').pack()
+        tk.Label(self.main_frame, text=f'время: {self.total_time}').pack()
+        tk.Button(self.main_frame, text='заново', command=self.start()).pack()
 
 
 
